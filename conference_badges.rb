63673137d1d0d4cdcfad4e921ba3c3118e825c41
Burbage Stacey <rstacey06@gmail.com>
2019-11-06 15:37:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+attendees = ["Edsger, Ada, Charles, Alan, Grace, Linus, Matz"]
+
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map {|attendees| "Hello, my name is #{attendees}."}
+end
+
+  def assign_rooms(attendees)
+    
