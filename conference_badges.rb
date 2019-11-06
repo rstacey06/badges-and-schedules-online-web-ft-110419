a7ca9869b728ea9380c3eaa6240ab0a6f7b138d0
Burbage Stacey <rstacey06@gmail.com>
2019-11-06 17:14:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,6 @@ end
 
 def assign_rooms(attendees)
   attendees.each_with_index.map do |attendee, index|
-    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+    puts "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
